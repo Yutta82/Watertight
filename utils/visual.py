46645c07d3076b37d4ab1fcd7daf3aa8cc5df539
yutta <2091921193@qq.com>
2025-11-12 14:13:11 +0800

    mesh info comment

--- utils/visual.py
+++ utils/visual.py
@@ -124,4 +124,9 @@ def show_two_meshes_with_holes(mesh1, mesh2, hole_loops=None,
     vedo.show([plots1, plots2], N=2, axes=1, bg='white', size=(1600, 800))
 
 def show_mesh_info(mesh: trimesh.Trimesh):
+    """
+    返回网格的基本信息
+    :param mesh:
+    :return: [顶点数量，边数量，面数量]
+    """
     return [len(mesh.vertices), len(mesh.edges_unique), len(mesh.faces)]
